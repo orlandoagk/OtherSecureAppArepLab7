76f44b73b0b731c5e535d9418318bd24cfa31301
Orlando Gelves <orlandoantoni@hotmail.com>
2020-10-01 23:58:05 -0500

    Resolving trutstore

--- src/main/java/edu/escuelaing/arep/secureapp/Main.java
+++ src/main/java/edu/escuelaing/arep/secureapp/Main.java
@@ -7,7 +7,7 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         port(433);
-        secure("ecikeystore2.p12","prueba",null,null);
+        secure("ecikeystore2.p12","prueba","myTrustStore","prueba");
         post("/",(req,res)->{
             JSONObject jsonObject = new JSONObject(req.body());
             return Calculator.calculate(jsonObject.get("operation").toString(),Double.parseDouble(jsonObject.get("value").toString()));
